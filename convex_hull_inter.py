efa166a972acc3b84e53be316bcffcb8eded165a
eduardo-ufmg <eduardohbc@ufmg.br>
2025-07-01 16:17:12 -0300

    Remove scaling factors from volume calculation in convex_hull_inter function

--- convex_hull_inter.py
+++ convex_hull_inter.py
@@ -121,7 +121,7 @@ def convex_hull_inter(
     try:
         # The volume is calculated from the convex hull of the intersection's vertices.
         final_hull = ConvexHull(intersection_vertices, qhull_options="QJ")
-        return float(final_hull.volume) * factor_h * factor_k
+        return float(final_hull.volume)
     except Exception:
         # This final hull computation can fail if the intersection vertices are
         # themselves co-planar, meaning the intersection is a degenerate object.
